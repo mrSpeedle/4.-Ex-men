70a7e2af4a54e0f40668c9ef1e5f516505571478
Sion <mr.speedle@yahoo.cl>
2024-07-17 20:03:37 -0400

    Tercer commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
-//Darle click a las tarjetitas y se retraen -_-
+//redi function
 
 $(document).ready(function () {
+    //Darle click a las tarjetitas y se retraen -_-
     $(".card .icon").on("click", function () {
         $(this).closest(".card").find(".card-body").slideToggle("fast");
+    });
+
+    $("#destacados .card").on("click", function () {
+        let $cardBody = $(this).find(".card-body");
+        $cardBody.slideToggle("fast");
     });
 
     $(".card .icon-bottom").on("click", function () {
@@ -14,12 +20,17 @@ $(document).ready(function () {
         }
     });
 
-    $(".card .icon, .card .icon-bottom, .card .card-img-top").on("click", function () {
-        let $cardBody = $(this).closest(".card").find(".card-body");
-        if ($cardBody.is(":visible")) {
-            $cardBody.slideUp("fast");
+    // Función para validar el formulario y mostrar alertas :v
+    $("#contacto button").on("click", function (event) {
+        event.preventDefault();
+        let nombre = $("#contacto input[placeholder='¿Cuál es tu nombre?']").val();
+        let asunto = $("#contacto input[placeholder='¿Cuál es el asunto?']").val();
+        let mensaje = $("#contacto textarea[placeholder='Escribe aquí tu mensaje']").val();
+
+        if (nombre && asunto && mensaje) {
+            alert("Correo enviado");
         } else {
-            $cardBody.slideDown("fast");
+            alert("Complete los datos del formulario");
         }
     });
 });
